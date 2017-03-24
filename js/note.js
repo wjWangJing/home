3e5a0e24e32ca71722338413601c917ba5a4e05d
eternal <18801411988@163.com>
2017-03-24 23:04:21 +0800

    note.html   responsive

--- js/note.js
+++ js/note.js
@@ -4,21 +4,26 @@
     * */
     var init = function(){
         var domElement = {
-            $noteTimeLine : document.getElementById("noteTimeLine"),
-            $timeLineTpl  : document.getElementById("timeLineTpl"),
-            $noteContent  : document.getElementById("noteContent"),
-            $contentTpl   : document.getElementById("contentTpl"),
-            $noteTop      : document.getElementById("noteTop")
+            $noteTimeLine   : document.getElementById("noteTimeLine"),
+            $timeLineTpl    : document.getElementById("timeLineTpl"),
+            $noteContent    : document.getElementById("noteContent"),
+            $contentMonthTpl: document.getElementById("contentMonthTpl"),
+            $noteTop        : document.getElementById("noteTop"),
+            $contentYearTpl : document.getElementById("contentYearTpl")
         };
-        updateTimeLine(domElement);
+        showTimeLine(domElement);
         updateContent(domElement);
         fixTimeLine(domElement);
+        showYear();
+        showMonth();
+        // windowResize(domElement);
+
     };
     /*
      * 更新内容区域的左侧时间轴
      * @params {object} domElement 要操作的dom元素
      * */
-    var updateTimeLine = function(domElement){
+    var showTimeLine = function(domElement){
         var _yearHtml = domElement.$noteTimeLine.innerHTML;
         var _monthHtml = domElement.$timeLineTpl.innerHTML;
         var yearLineHtml = [];
@@ -34,63 +39,213 @@
             yearLineHtml.unshift(yearHtml.replace(/\{\{monthList\}\}/g,monthLineHtml.join("")))
         }
         domElement.$noteTimeLine.innerHTML = yearLineHtml.join("");
-        console.log(yearLineHtml);
     };
     /*
      * 更新内容区域的右侧内容列表
      * @params {object} domElement 要操作的dom元素
      * */
     var updateContent = function(domElement){
-        var _contentTpl = domElement.$contentTpl.innerHTML;
-        var _contentHtml = domElement.$noteContent.innerHTML;
-        var contentHtmlArr = [];
-        for(var item in noteData){
-            for(var index in noteData[item]){
-                var flag = false;
-                var contentMonthItem = [];
-                for(var i in noteData[item][index]){
-                    var contentHtml = _contentHtml
-                        .replace(/\{\{title\}\}/g,noteData[item][index][i].title)
-                        .replace(/\{\{date\}\}/g,noteData[item][index][i].date)
-                        .replace(/\{\{summary\}\}/g,noteData[item][index][i].summary)
-                        .replace(/\{\{className\}\}/g,i%2?"note-content-item-right":"note-content-item-left")
-                        .replace(/\{\{pointClassName\}\}/g,flag?"note-content-point-left":"")
-                        .replace(/\{\{href\}\}/g,noteData[item][index][i].href)
-                        .replace(/\{\{year\}\}/g,item)
-                        .replace(/\{\{month\}\}/g,index);
-                    contentMonthItem.push(contentHtml);
-                    flag = !flag;
+        var _contentYearTpl = domElement.$contentYearTpl.innerHTML;
+        var _contentMonthTpl = domElement.$contentMonthTpl.innerHTML;
+        var _contentMonthItemTpl = domElement.$noteContent.innerHTML;
+        var contentHtml = [];
+        for(var year in noteData){
+            var contentYear = [];
+            for(var month in noteData[year]){
+                var contentMonth = [];
+                for(var date in noteData[year][month]){
+                    var contentMonthItem = _contentMonthItemTpl
+                        .replace(/\{\{title\}\}/g,noteData[year][month][date].title)
+                        .replace(/\{\{date\}\}/g,noteData[year][month][date].date)
+                        .replace(/\{\{summary\}\}/g,noteData[year][month][date].summary)
+                        .replace(/\{\{className\}\}/g,date%2?"note-content-item-right":"note-content-item-left")
+                        .replace(/\{\{href\}\}/g,noteData[year][month][date].href)
+                        .replace(/\{\{year\}\}/g,year)
+                        .replace(/\{\{month\}\}/g,month);
+                    contentMonth.push(contentMonthItem);
                 }
-                var contentTplHtml = _contentTpl
-                        .replace(/\{\{list\}\}/g,contentMonthItem.join(""))
-                        .replace(/\{\{year\}\}/g,item)
-                        .replace(/\{\{month\}\}/g,index);
-                contentHtmlArr.unshift(contentTplHtml);
+                var contentMonthHtml = _contentMonthTpl
+                    .replace(/\{\{year\}\}/g,year)
+                    .replace(/\{\{month\}\}/g,month)
+                    .replace(/\{\{list\}\}/g,contentMonth.join(""));
+                contentYear.unshift(contentMonthHtml)
             }
-            domElement.$noteContent.innerHTML = contentHtmlArr.join("");
+            var contentYearHtml = _contentYearTpl
+                .replace(/\{\{yearList\}\}/g,contentYear.join(""))
+                .replace(/\{\{year\}\}/g,year);
+            contentHtml.unshift(contentYearHtml);
+            domElement.$noteContent.innerHTML = contentHtml.join("");
         }
-        console.log(contentHtmlArr);
     };
 
     /*
-     * 固定左侧时间轴
+     * 滚动条事件
      * @params {object} domElement 要操作的dom元素
      * */
     var fixTimeLine = function(domElement){
         window.onscroll = function () {
             var scrollTop = document.body.scrollTop;
-            var height = domElement.$noteTop.offsetHeight + 40;
-            if(scrollTop >= height){
-                domElement.$noteTimeLine.style.position = "fixed";
-                domElement.$noteTimeLine.style.top = "10px";
-                domElement.$noteTimeLine.style.left = "8%";
-            }else{
-                domElement.$noteTimeLine.style.position = "";
-                domElement.$noteTimeLine.style.top = "";
-                domElement.$noteTimeLine.style.left = "";
+            var height = domElement.$noteTop.offsetHeight + 30;
+            var bodyWidth = document.body.clientWidth;
+            var parentWidth = document.getElementsByClassName("note-bottom")[0].offsetWidth;
+            if(bodyWidth <= 960 && bodyWidth > 800){
+                if(scrollTop >= height){
+                    domElement.$noteTimeLine.style.position = "fixed";
+                    domElement.$noteTimeLine.style.top = "10px";
+                    domElement.$noteTimeLine.style.left = bodyWidth * 0.05 + "px";
+                    domElement.$noteTimeLine.style.width = parentWidth * 0.15 + "px";
+                }else{
+                    domElement.$noteTimeLine.style.position = "";
+                    domElement.$noteTimeLine.style.top = "";
+                    domElement.$noteTimeLine.style.left = "";
+                    domElement.$noteTimeLine.style.width = "";
+                }
+            }else if(bodyWidth > 960){
+                if(scrollTop >= height){
+                    domElement.$noteTimeLine.style.position = "fixed";
+                    domElement.$noteTimeLine.style.top = "10px";
+                    domElement.$noteTimeLine.style.left = "8%";
+                }else{
+                    domElement.$noteTimeLine.style.position = "";
+                    domElement.$noteTimeLine.style.top = "";
+                    domElement.$noteTimeLine.style.left = "";
+                }
+            }else if(bodyWidth <= 800){
+                if(scrollTop >= height){
+                    domElement.$noteTimeLine.style.position = "fixed";
+                    domElement.$noteTimeLine.style.top = "10px";
+                    domElement.$noteTimeLine.style.left = bodyWidth * 0.025 + "px";
+                    domElement.$noteTimeLine.style.width = parentWidth * 0.15 + "px";
+                }else{
+                    domElement.$noteTimeLine.style.position = "";
+                    domElement.$noteTimeLine.style.top = "";
+                    domElement.$noteTimeLine.style.left = "";
+                    domElement.$noteTimeLine.style.width = "";
+                }
+            }
+
+            updateTimeLineYear(scrollTop);
+            updateTimeLineMonth(scrollTop);
+        }
+    };
+    /*
+     * 左侧时间轴--年份点击处理
+     * */
+    var showYear = function(){
+        for(var year in noteData){
+            (function(year){
+                var content_year = document.getElementById("note-content-year-"+year);
+                var timeLine_year = document.getElementById("note_timeLine_year_"+year);
+                openMonthList(year,timeLine_year);
+                timeLine_year.onclick = function(){
+                    openMonthList(year,timeLine_year);
+                    var height = content_year.offsetTop + 120;
+                    var start = document.body.scrollTop;
+                    window.scrollTo(start,height);
+                }
+            })(year);
+        }
+    };
+    /*
+     * 左侧时间轴--月份点击处理
+     * */
+    var showMonth = function(){
+        for(var year in noteData){
+            for(var month in noteData[year]){
+                (function(year,month){
+                    var content_year_month = document.getElementById("note_content_year_"+year+"_"+month);
+                    var timeLine_year_month = document.getElementById("note_timeLine_year_"+year+"_"+month);
+                    timeLine_year_month.onclick = function(){
+                        var height = content_year_month.offsetTop + 130;
+                        var start = document.body.scrollTop;
+                        window.scrollTo(start,height);
+                    }
+                })(year,month);
             }
         }
-
+    };
+    /*
+     * 左侧时间轴--点击年份展开月份
+     * */
+    var openMonthList = function (year,element) {
+        var years = document.getElementById("noteTimeLine").getElementsByClassName("year-list");
+        var months = document.getElementById("noteTimeLine").getElementsByClassName("month-list");
+        var currentYear_month = document.getElementById("noteTimeLine").getElementsByClassName(year+"_month");
+        // 清除所有年份的current样式
+        for(var i = 0; i < years.length; i++){
+            years[i].className = years[i].className.replace(/current/,"");
+        }
+        // 隐藏所有月份
+        for(var j = 0; j < months.length; j++){
+            months[j].style.display = "none";
+        }
+        // 展示当前年份下的月份
+        for(var k = 0; k < currentYear_month.length; k++){
+            currentYear_month[k].style.display = "block";
+        }
+        element.className += " current";
+    };
+    /*
+    * 根据滚动条的位置更新左侧时间轴的年月的显示隐藏
+    * */
+    var updateTimeLineYear = function(scrollTop){
+        var years = document.getElementById("noteContent").getElementsByClassName("note-content-year");
+        var scrollTopYear = [];
+        for(var i = 0; i < years.length; i++){
+            scrollTopYear.push(years[i].offsetTop)
+        }
+        for(var j = 1; j < scrollTopYear.length; j++){
+            if(scrollTop < scrollTopYear[j] && scrollTop > scrollTopYear[j - 1]){
+                var year = years[i-1].innerHTML.slice(0,4);
+                openMonthList(year,document.getElementById("note_timeLine_year_"+year));
+                return ;
+            }
+        }
+    };
+    /*
+    * 根据滚动条的位置高亮月份
+    * */
+    var updateTimeLineMonth = function(scrollTop){
+        var months = document.getElementsByClassName("note-content-month");
+        var scrollTopMonth = [];
+        for(var i = 0; i < months.length; i++){
+            scrollTopMonth.push(months[i].offsetTop)
+        }
+        for(var j = 1; j < scrollTopMonth.length; j++){
+            if(scrollTop < scrollTopMonth[j] && scrollTop > scrollTopMonth[j - 1]){
+                var year = months[j-1].id.split("_")[3];
+                var month = months[j-1].id.split("_")[4];
+                monthHighLight(year,month,document.getElementById("note_timeLine_year_"+year+"_"+month));
+                return ;
+            }
+        }
+        console.log("scrollTopMonth",scrollTopMonth)
+    };
+    /*
+    * 滚动条的位置和月份对应
+    * */
+    var monthHighLight = function (year,month,element) {
+        var months = document.getElementsByClassName(year+"_month");
+        var scrollTopMonth = [];
+        // 清除所有月份current样式
+        for(var k = 0; k < months.length; k++){
+            months[k].className = months[k].className.replace(/current/,"");
+        }
+        for(var i = 0; i < months.length; i++){
+            scrollTopMonth.push(months[i].scrollTop);
+        }
+        for(var j = 1; j < scrollTopMonth.length; j++){
+            openMonthList(year,document.getElementById("note_timeLine_year_"+year));
+        }
+        element.className += " current";
+    };
+    /*
+     * 窗口改变事件处理; 保持时序列表的位置
+     * */
+    var windowResize = function(domElement){
+        window.onresize = function(){
+            fixTimeLine(domElement)
+        }
     };
 
     init();
--- js/noteData.js
+++ js/noteData.js
@@ -12,6 +12,300 @@ var noteData = {
                 "title" : "前端在线工具",
                 "summary" : "一些有趣又实用的在线工具",
                 "href" : "http://www.jianshu.com/p/9326614004b0"
+            },
+            {
+                "date" : "2017.03.02",
+                "title" : "前端在线工具",
+                "summary" : "一些有趣又实用的在线工具",
+                "href" : "http://www.jianshu.com/p/9326614004b0"
+            }
+        ],
+        2 : [
+            {
+                "date" : "2017.02.15",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            }
+        ],
+        1 : [
+            {
+                "date" : "2017.01.15",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            }
+        ]
+    },
+    2016 : {
+        12 : [
+            {
+                "date" : "2016.12.25",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },
+            {
+                "date" : "2016.12.24",
+                "title" : "前端在线工具",
+                "summary" : "一些有趣又实用的在线工具",
+                "href" : "http://www.jianshu.com/p/9326614004b0"
+            },
+            {
+                "date" : "2016.12.15",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },
+            {
+                "date" : "2016.12.04",
+                "title" : "前端在线工具",
+                "summary" : "一些有趣又实用的在线工具",
+                "href" : "http://www.jianshu.com/p/9326614004b0"
+            }
+        ],
+        11: [
+            {
+                "date" : "2016.11.15",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },
+            {
+                "date" : "2016.11.10",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },{
+                "date" : "2016.11.02",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            }
+        ],
+        10 : [
+            {
+                "date" : "2016.10.25",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },
+            {
+                "date" : "2016.10.24",
+                "title" : "前端在线工具",
+                "summary" : "一些有趣又实用的在线工具",
+                "href" : "http://www.jianshu.com/p/9326614004b0"
+            },
+            {
+                "date" : "2016.10.15",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },
+            {
+                "date" : "2016.10.04",
+                "title" : "前端在线工具",
+                "summary" : "一些有趣又实用的在线工具",
+                "href" : "http://www.jianshu.com/p/9326614004b0"
+            }
+        ],
+        9: [
+            {
+                "date" : "2016.09.15",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },
+            {
+                "date" : "2016.09.10",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },{
+                "date" : "2016.09.02",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            }
+        ],
+        8 : [
+            {
+                "date" : "2016.08.25",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },
+            {
+                "date" : "2016.08.24",
+                "title" : "前端在线工具",
+                "summary" : "一些有趣又实用的在线工具",
+                "href" : "http://www.jianshu.com/p/9326614004b0"
+            },
+            {
+                "date" : "2016.08.15",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },
+            {
+                "date" : "2016.08.04",
+                "title" : "前端在线工具",
+                "summary" : "一些有趣又实用的在线工具",
+                "href" : "http://www.jianshu.com/p/9326614004b0"
+            }
+        ],
+        7: [
+            {
+                "date" : "2016.07.15",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },
+            {
+                "date" : "2016.07.10",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },{
+                "date" : "2016.07.02",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            }
+        ],
+        6 : [
+            {
+                "date" : "2016.06.25",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },
+            {
+                "date" : "2016.06.24",
+                "title" : "前端在线工具",
+                "summary" : "一些有趣又实用的在线工具",
+                "href" : "http://www.jianshu.com/p/9326614004b0"
+            },
+            {
+                "date" : "2016.06.15",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },
+            {
+                "date" : "2016.06.04",
+                "title" : "前端在线工具",
+                "summary" : "一些有趣又实用的在线工具",
+                "href" : "http://www.jianshu.com/p/9326614004b0"
+            }
+        ],
+        5: [
+            {
+                "date" : "2016.05.15",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },
+            {
+                "date" : "2016.05.10",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },{
+                "date" : "2016.05.02",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            }
+        ],
+        4 : [
+            {
+                "date" : "2016.04.25",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },
+            {
+                "date" : "2016.04.24",
+                "title" : "前端在线工具",
+                "summary" : "一些有趣又实用的在线工具",
+                "href" : "http://www.jianshu.com/p/9326614004b0"
+            },
+            {
+                "date" : "2016.04.15",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },
+            {
+                "date" : "2016.04.04",
+                "title" : "前端在线工具",
+                "summary" : "一些有趣又实用的在线工具",
+                "href" : "http://www.jianshu.com/p/9326614004b0"
+            }
+        ],
+        3: [
+            {
+                "date" : "2016.03.15",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },
+            {
+                "date" : "2016.03.10",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },{
+                "date" : "2016.03.02",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            }
+        ],
+        2 : [
+            {
+                "date" : "2016.02.25",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },
+            {
+                "date" : "2016.02.24",
+                "title" : "前端在线工具",
+                "summary" : "一些有趣又实用的在线工具",
+                "href" : "http://www.jianshu.com/p/9326614004b0"
+            },
+            {
+                "date" : "2016.02.15",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },
+            {
+                "date" : "2016.02.04",
+                "title" : "前端在线工具",
+                "summary" : "一些有趣又实用的在线工具",
+                "href" : "http://www.jianshu.com/p/9326614004b0"
+            }
+        ],
+        1: [
+            {
+                "date" : "2016.01.15",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },
+            {
+                "date" : "2016.01.10",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
+            },{
+                "date" : "2016.01.02",
+                "title" : "资源链接",
+                "summary" : "一些在线文档和手册，方便查询和参考",
+                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
             }
         ]
     }
