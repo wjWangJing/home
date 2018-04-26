19870b877d1bd81d1c6bc8e6a9284abf26b20cbf
wangjing <18801411988@163.com>
2018-04-26 16:14:32 +0800

    update

--- js/noteData.js
+++ js/noteData.js
@@ -1,209 +1,77 @@
 var noteData = {
-    2017 : {
-        4 : [
-            {
-                "date" : "2017.04.13",
-                "title" : "在浏览器中输入网址到打开网页的过程",
-                "summary" : "在浏览器中输入网址到打开网页的过程都发生了些什么呢？一起来看看吧",
-                "href" : "http://www.jianshu.com/p/8b26caa32694"
-            }
-        ],
-        3 : [
-            {
-                "date" : "2017.03.15",
-                "title" : "资源链接",
-                "summary" : "一些在线文档和手册，方便查询和参考",
-                "href" : "http://www.jianshu.com/p/4c0321f1b3ce"
-            },
-            {
-                "date" : "2017.03.04",
-                "title" : "前端在线工具",
-                "summary" : "一些有趣又实用的在线工具",
-                "href" : "http://www.jianshu.com/p/9326614004b0"
-            }
-        ],
-        2 : [
-            {
-                "date" : "2017.02.15",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            },
-            {
-                "date" : "2017.02.04",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            },
-            {
-                "date" : "2017.02.02",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            }
-        ]
-    },
-    2016 : {
-        12 : [
-            {
-                "date" : "2016.12.25",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            },
-            {
-                "date" : "2016.12.24",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            },
-            {
-                "date" : "2016.12.15",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            },
-            {
-                "date" : "2016.12.04",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            }
-        ],
-        11: [
-            {
-                "date" : "2016.11.15",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            },
-            {
-                "date" : "2016.11.10",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            },{
-                "date" : "2016.11.02",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            }
-        ],
-        10 : [
-            {
-                "date" : "2016.10.25",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            },
-            {
-                "date" : "2016.10.24",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            },
-            {
-                "date" : "2016.10.15",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            },
-            {
-                "date" : "2016.10.04",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            }
-        ],
-        9: [
-            {
-                "date" : "2016.09.15",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            },
-            {
-                "date" : "2016.09.10",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            },{
-                "date" : "2016.09.02",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            }
-        ],
-        8 : [
-            {
-                "date" : "2016.08.25",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            },
-            {
-                "date" : "2016.08.24",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            },
-            {
-                "date" : "2016.08.15",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            },
-            {
-                "date" : "2016.08.04",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            }
-        ],
-        7: [
-            {
-                "date" : "2016.07.15",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            },
-            {
-                "date" : "2016.07.10",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            },{
-                "date" : "2016.07.02",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            }
-        ],
-        6 : [
-            {
-                "date" : "2016.06.25",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            },
-            {
-                "date" : "2016.06.24",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            },
-            {
-                "date" : "2016.06.15",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            },
-            {
-                "date" : "2016.06.04",
-                "title" : "测试数据",
-                "summary" : "测试",
-                "href" : "javascript:void(0)"
-            }
-        ]
-    }
+	2018: {
+		4: [{
+				"date": "2018.04.25",
+				"title": "【原创】hexo系列问题",
+				"summary": "用hexo搞了个自己的博客，顺便记录一下自己踩过的坑",
+				"href": "https://wjwangjing.github.io/blog/2018/04/25/%E3%80%90%E5%8E%9F%E5%88%9B%E3%80%91hexo%E7%B3%BB%E5%88%97%E9%97%AE%E9%A2%98/"
+			}, {
+				"date": "2018.04.24",
+				"title": "【原创】微信小程序开发遇到的问题",
+				"summary": "最近在开发微信小程序，顺便记录一下自己踩过的坑",
+				"href": "https://wjwangjing.github.io/blog/2018/04/24/%E3%80%90%E5%8E%9F%E5%88%9B%E3%80%91%E5%BE%AE%E4%BF%A1%E5%B0%8F%E7%A8%8B%E5%BA%8F%E5%BC%80%E5%8F%91%E9%81%87%E5%88%B0%E7%9A%84%E9%97%AE%E9%A2%98/"
+			}
+
+		]
+	},
+	2017: {
+		10: [{
+				"date": "2017.10.20",
+				"title": "【原创】关于git操作",
+				"summary": "在实际工作中总会有一些不常用的git命令，但是用的时候却又到处找手册，在这篇博文中，记录了一些我偶尔用的git命令，方便查找",
+				"href": "https://wjwangjing.github.io/blog/2017/10/20/%E3%80%90%E5%8E%9F%E5%88%9B%E3%80%91%E5%85%B3%E4%BA%8Egit%E6%93%8D%E4%BD%9C/"
+			},
+			{
+				"date": "2017.09.27",
+				"title": "【原创】UC浏览器中不显示img图片",
+				"summary": "本文主要介绍UC浏览器中不显示img的问题，主要关注的是解决问题的思路",
+				"href": "https://wjwangjing.github.io/blog/2017/09/27/%E3%80%90%E5%8E%9F%E5%88%9B%E3%80%91UC%E6%B5%8F%E8%A7%88%E5%99%A8%E4%B8%AD%E4%B8%8D%E6%98%BE%E7%A4%BAimg%E5%9B%BE%E7%89%87/"
+			}
+		],
+		9: [{
+				"date": "2017.09.27",
+				"title": "【原创】UC浏览器中不显示img图片",
+				"summary": "本文主要介绍UC浏览器中不显示img的问题，主要关注的是解决问题的思路",
+				"href": "https://wjwangjing.github.io/blog/2017/09/27/%E3%80%90%E5%8E%9F%E5%88%9B%E3%80%91UC%E6%B5%8F%E8%A7%88%E5%99%A8%E4%B8%AD%E4%B8%8D%E6%98%BE%E7%A4%BAimg%E5%9B%BE%E7%89%87/"
+			}, {
+				"date": "2017.09.25",
+				"title": "【原创】前端开发过程中遇到的问题及解决方法",
+				"summary": "记录一下工作中遇到的问题，并附上解决方案",
+				"href": "https://wjwangjing.github.io/blog/2017/09/25/%E3%80%90%E5%8E%9F%E5%88%9B%E3%80%91%E5%89%8D%E7%AB%AF%E5%BC%80%E5%8F%91%E8%BF%87%E7%A8%8B%E4%B8%AD%E9%81%87%E5%88%B0%E7%9A%84%E9%97%AE%E9%A2%98%E5%8F%8A%E8%A7%A3%E5%86%B3%E6%96%B9%E6%B3%95/"
+			}
+
+		],
+		8: [{
+				"date": "2017.08.29",
+				"title": "【原创】js上传图片到七牛云存储",
+				"summary": "项目开发过程中遇到一个需求，运营人员需要上传图片到七牛云，最开始的做法是，后台对接七牛，然后出一个接口，前端调用接口，先将图片传到后台，然后后台再上传七牛云...",
+				"href": "https://wjwangjing.github.io/blog/2017/08/29/%E3%80%90%E5%8E%9F%E5%88%9B%E3%80%91js%E4%B8%8A%E4%BC%A0%E5%9B%BE%E7%89%87%E5%88%B0%E4%B8%83%E7%89%9B%E4%BA%91%E5%AD%98%E5%82%A8/"
+			},
+			{
+				"date": "2017.08.29",
+				"title": "【原创】移动端浏览器隐私模式或者无痕模式会影响localStorage/sessionStorage使用",
+				"summary": "有没有被某些浏览器的隐私模式搞懵过？隐私模式会导致各种各样的问题，比如常用的本地存储localStorage/sessionStorage",
+				"href": "https://wjwangjing.github.io/blog/2017/08/29/%E3%80%90%E5%8E%9F%E5%88%9B%E3%80%91%E7%A7%BB%E5%8A%A8%E7%AB%AF%E6%B5%8F%E8%A7%88%E5%99%A8%E9%9A%90%E7%A7%81%E6%A8%A1%E5%BC%8F%E6%88%96%E8%80%85%E6%97%A0%E7%97%95%E6%A8%A1%E5%BC%8F%E4%BC%9A%E5%BD%B1%E5%93%8DlocalStorage-sessionStorage%E4%BD%BF%E7%94%A8/"
+			}, {
+				"date": "2017.08.28",
+				"title": "【原创】如何使用电脑上的谷歌浏览器来调试移动端页面",
+				"summary": "参照文章解决问题的前提是手机和电脑上都需要安装谷歌浏览器",
+				"href": "https://wjwangjing.github.io/blog/2017/08/28/%E3%80%90%E5%8E%9F%E5%88%9B%E3%80%91%E5%A6%82%E4%BD%95%E4%BD%BF%E7%94%A8%E7%94%B5%E8%84%91%E4%B8%8A%E7%9A%84%E8%B0%B7%E6%AD%8C%E6%B5%8F%E8%A7%88%E5%99%A8%E6%9D%A5%E8%B0%83%E8%AF%95%E7%A7%BB%E5%8A%A8%E7%AB%AF%E9%A1%B5%E9%9D%A2/"
+			},
+			{
+				"date": "2017.08.28",
+				"title": "【原创】在mac上如何用safari调试ios手机的移动端页面",
+				"summary": "移动端网页调试比较麻烦，在此记录一种方法，方便查找",
+				"href": "https://wjwangjing.github.io/blog/2017/08/28/%E3%80%90%E5%8E%9F%E5%88%9B%E3%80%91%E5%9C%A8mac%E4%B8%8A%E5%A6%82%E4%BD%95%E7%94%A8safari%E8%B0%83%E8%AF%95ios%E6%89%8B%E6%9C%BA%E7%9A%84%E7%A7%BB%E5%8A%A8%E7%AB%AF%E9%A1%B5%E9%9D%A2/"
+			},
+
+		],
+		4: [{
+			"date": "2017.04.13",
+			"title": "在浏览器中输入网址到打开网页的过程",
+			"summary": "本文以chrome为例，介绍了在浏览器中输入网址到打开网页的过程，记录流程，并没有深究太多",
+			"href": "https://wjwangjing.github.io/blog/2017/04/13/%E3%80%90%E8%BD%AC%E8%BD%BD%E3%80%91%E5%9C%A8%E6%B5%8F%E8%A7%88%E5%99%A8%E4%B8%AD%E8%BE%93%E5%85%A5%E7%BD%91%E5%9D%80%E5%88%B0%E6%89%93%E5%BC%80%E7%BD%91%E9%A1%B5%E7%9A%84%E8%BF%87%E7%A8%8B/"
+		}],
+	},
+
 };
